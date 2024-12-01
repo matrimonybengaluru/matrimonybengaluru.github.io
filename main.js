47d515f77f28aed961fe2ae01d213e67625ec43d
Singh, Atul Kumar <Atul.Singh@kurtzersa.de>
2024-12-01 22:18:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// main.ts
+document.addEventListener("DOMContentLoaded", function () {
+    var greeting = document.createElement("h4");
+    greeting.textContent = "Brought together by Ummehani and Aktharunnisa.";
+    document.body.appendChild(greeting);
+});
---
+++ main.ts
@@ -0,0 +1,6 @@
+// main.ts
+document.addEventListener("DOMContentLoaded", () => {
+    const greeting = document.createElement("h4");
+    greeting.textContent = "Brought together by Ummehani and Aktharunnisa.";
+    document.body.appendChild(greeting);
+});
